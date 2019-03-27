caa2ccd7fdc92fb2def9cd71095bc757506f4852
Julia Rudi <49004261+JuliaRudi13@users.noreply.github.com>
2019-03-28 00:20:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf('807177629:AAHtH5tgegM33_HZg8H0NxaK2G28BQAlFmw')
+const bot = new Telegraf('тут был мой токен')
 bot.start((ctx) => ctx.reply('Привет'))
 bot.help((ctx) => ctx.reply('Никакой помощи от бога помощи'))
 bot.on('message', (ctx) => ctx.reply(ctx.message.text))
